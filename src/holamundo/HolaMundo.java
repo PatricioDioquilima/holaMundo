d4e35a1df640781d2e5592e91137b65e30f9e8ac
patricio dioquilima <patricio_76@outlook.com>
2021-11-06 11:34:06 -0500

    he agregado un hola GitHub

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Pato
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Hola mundo con GitHub 
+        System.out.println("Hola GitHub " );
+    }
+    
+}
